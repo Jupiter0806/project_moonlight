944bb7c4af4b75cf12903c29c77f30463facac15
Molten Tigerx <jupiterli0806@gmail.com>
2026-04-05 22:13:02 +1000

    Feature/complete ci (#10)
    * fix(ci): correct workflow paths for pull request and push triggers
    
    * test: Add testing setup and SignOutButton component tests
    
    * feat(ci): add unit test job to CI workflow
    
    * fix(ci): update actions/checkout and pnpm/action-setup to version 5
    
    * fix(ci): update githug actions latest versions; add .nvmrc and set to 22
    
    * fix(ci): update node version file path to web/.nvmrc in CI workflow
    
    * fix(next.config): comment out unused path imports and output settings
    
    * feat(ci): add dependabot configuration and enhance CI workflow with concurrency and permissions
    
    * feat(ci): add security job for CodeQL analysis and dependency auditing
    
    * feat(ci): enhance CI workflow documentation with automated dependency updates and validation steps
    
    * fix(ci): correct file path casing in web CI workflow and update CodeQL action versions
    
    * fix(ci): update permissions for security job to include actions and pull-requests
    
    * feat(ci): replace CodeQL analysis with Semgrep SAST
    
    uploading CodeQL results requires Github enterprise license

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,9 +1,9 @@
-import path from "path";
+// import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  outputFileTracingRoot: path.join(__dirname),
+  // output: "standalone",
+  // outputFileTracingRoot: path.join(__dirname),
   reactCompiler: true,
 };
 
