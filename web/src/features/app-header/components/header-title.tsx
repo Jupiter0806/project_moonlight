38402778cf3ea8cd9ebcf3e0e23d1bb354169e84
Molten Tigerx <jupiterli0806@gmail.com>
2026-04-23 16:19:52 +1000

    feat: refactor header components to use pathname for title and options

--- web/src/features/app-header/components/header-title.tsx
+++ web/src/features/app-header/components/header-title.tsx
@@ -1,19 +1,9 @@
 "use client";
 
-import { useRoute } from "@/hooks/use-route";
+import { getRouteName } from "@/lib/routes";
+import { usePathname } from "next/navigation";
 
 export function HeaderTitle() {
-  const route = useRoute();
-
-  return <div className="text-lg font-semibold">{mapRouteToTitle(route)}</div>;
-}
-
-function mapRouteToTitle(route: string) {
-  // You can implement more complex logic here to map different routes to different titles
-  if (route === "dashboard") {
-    return "Dashboard";
-  } else if (route === "settings") {
-    return "Settings";
-  }
-  return "Chamber"; // Default title
+  const pathname = usePathname();
+  return <div className="text-lg font-semibold">{getRouteName(pathname)}</div>;
 }
--- web/src/lib/routes.ts
+++ web/src/lib/routes.ts
@@ -2,3 +2,12 @@ export const routes = [
   { path: "/", name: "Chamber" },
   { path: "/camphor", name: "Camphor" },
 ];
+
+export function getRouteName(path: string) {
+  const route = routes.find((route) => route.path === path);
+  return route ? route.name : "Chamber";
+}
+
+export function getAllRoutePaths() {
+  return routes.map((route) => route.path);
+}
